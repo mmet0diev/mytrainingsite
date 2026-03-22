100050981c0552b3ef3eb6b3fd37e7e48b90e4b0
mmet0diev <max.metodiev.1999@gmail.com>
2026-03-22 11:46:59 +0200

    solving bash challenges on ahckerrank

---
+++ hackrank.bash
@@ -0,0 +1,20 @@
+# Cut 1 (initial solution)
+# read n
+# for ((i=0; i<$n; i++)); do
+#     read
+#     echo $REPLY | cut -c 3
+# done
+
+
+# CUT 1
+# while read
+# do
+#     echo $REPLY | cut -c 3
+# done
+
+
+# CUT 2
+# while read
+# do
+#     echo $REPLY | cut -c 2,7
+# done
