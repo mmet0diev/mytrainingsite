0b5a26db560047fbc0281de0ffdcc078df486a9b
mmet0diev <max.metodiev.1999@gmail.com>
2026-04-15 16:50:55 +0300

    polar coords

--- hackrank.py
+++ hackrank.py
@@ -1,20 +1,22 @@
-def print_formatted(number):
-    # your code goes here
-    # Decimal
-    # Octal
-    # Hexadecimal(capitalized)
-    # Binary
-    output = ""
-    for i in range(1, number + 1):
-        width = len(str(bin(number))[2:])
-        output += str(i).rjust(width) + " "
-        output += str(oct(i))[2:].rjust(width) + " "
-        output += str(hex(i))[2:].upper().rjust(width) + " "
-        output += str(bin(i))[2:].rjust(width)
-        if i < number:
-            output += "\n"
+from cmath import *
 
-    print(output)
+# def print_formatted(number):
+#     # your code goes here
+#     # Decimal
+#     # Octal
+#     # Hexadecimal(capitalized)
+#     # Binary
+#     output = ""
+#     for i in range(1, number + 1):
+#         width = len(str(bin(number))[2:])
+#         output += str(i).rjust(width) + " "
+#         output += str(oct(i))[2:].rjust(width) + " "
+#         output += str(hex(i))[2:].upper().rjust(width) + " "
+#         output += str(bin(i))[2:].rjust(width)
+#         if i < number:
+#             output += "\n"
+
+#     print(output)
 
 
 # print("--------e--------\n"
@@ -27,31 +29,39 @@ def print_formatted(number):
 #           "------e-d-e------\n"
 #           "--------e--------\n")
 
-def print_rangoli(size):
-    # your code goes here
-    a = chr(97)
-    rangoli = ""
-    letters = []
+# def print_rangoli(size):
+#     # your code goes here
+#     a = chr(97)
+#     rangoli = ""
+#     letters = []
 
-    # add letters to list
-    for i in range(97, 97 + size):
-        letters.append(chr(i))
+#     # add letters to list
+#     for i in range(97, 97 + size):
+#         letters.append(chr(i))
 
-    letters1 = []
-    for i in range(97 + size - 1, 96, -1):
-        letters1.append(chr(i))
+#     letters1 = []
+#     for i in range(97 + size - 1, 96, -1):
+#         letters1.append(chr(i))
 
-    lets = ""
-    for l in letters:
-        for i in range(len(letters1)):
-            lets+=letters1[i]
+#     lets = ""
+#     for l in letters:
+#         for i in range(len(letters1)):
+#             lets+=letters1[i]
 
-        rangoli+=lets
-
-
-    print(letters)
-    print(letters1)
-    print(rangoli)
+#         rangoli+=lets
 
 
-print_rangoli(3)
+#     print(letters)
+#     print(letters1)
+#     print(rangoli)
+
+
+# print_rangoli(3)
+
+# Problem/Solution - Polar Coordinates
+n = input()
+
+complex_n = complex(n)
+
+print(abs(complex_n))
+print(phase(complex_n))
